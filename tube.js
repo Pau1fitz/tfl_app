d6500c02077651cb5d6df800cb186c839b4d8016
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-28 11:51:17 +0000

    first commit

---
+++ tube.js
@@ -0,0 +1,18 @@
+var tube = angular.module('tube',[]);
+
+tube.controller('tubeController', function($scope, $http) {
+
+   $scope.holder = [];
+
+  $http.get('http://transportapi.com/v3/uk/tube.json?api_key=d91966cd38ad23b8a95f759f7d983e62&app_id=14368b83')
+    .success(function(data){
+      $scope.lines = data.lines;
+       $.each($scope.lines, function(name){
+        $scope.holder.push(this.friendly_name);
+        $scope.holder.push(this.status);
+        console.log($scope.holder)
+      });
+  });
+});
+
+
