b004cca785896f0b7aae40799d441c383c90087e
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-29 13:20:20 +0100

    added regex to remove + from input

--- tube.js
+++ tube.js
@@ -6,7 +6,6 @@ tube.controller('tubeController', function($scope, $http) {
    $scope.destination = [];
    var i = 0;
 
-
   $http.get('http://transportapi.com/v3/uk/tube.json?api_key=d91966cd38ad23b8a95f759f7d983e62&app_id=14368b83')
     .success(function(data){
       $scope.lines = data.lines;
@@ -18,7 +17,8 @@ tube.controller('tubeController', function($scope, $http) {
 
 $scope.postcode = function(){
 
-  console.log($scope.postcode1 + " " + $scope.postcode2);
+   $scope.postcode1 = $scope.postcode1.replace(/ /g,"+");
+   $scope.postcode2 = $scope.postcode2.replace(/ /g,"+");
 
   $http.get('http://transportapi.com/v3/uk/public/journey/from/postcode:' + $scope.postcode1 + '/to/postcode:' + $scope.postcode2 + '.json?api_key=d91966cd38ad23b8a95f759f7d983e62&app_id=14368b83')
     .success(function(data){
